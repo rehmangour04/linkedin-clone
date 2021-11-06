971296353073c20c602685c056ed7dffd2f10377
rehman <rehmangour04@gmail.com>
2021-11-06 22:49:36 +0530

    building the post section

--- src/Feed.js
+++ src/Feed.js
@@ -1,32 +1,36 @@
-import React from 'react';
+import React from "react";
 import "./Feed.css";
-import CreateIcon from '@mui/icons-material/Create';
-import InputOption from './InputOption';
+import CreateIcon from "@mui/icons-material/Create";
+import InputOption from "./InputOption";
+import ImageIcon from "@mui/icons-material/Image";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import EventNoteIcon from "@mui/icons-material/EventNote";
+import CalendarViewDayIcon from "@mui/icons-material/CalendarViewDay";
 
 function Feed() {
-    return (
-        <div className="feed">
-       <div className="feed__inputContainer">
-       <div className="feed__input">
-           <CreateIcon/>
-           <form>
-           <input type="text" />
-           <button type="submit">Send</button>
-
-
-           </form>
-
-       </div>
-       <div  className="feed__inputOptions">
-       <InputOption/>
-
-
-       </div>
-
-       </div>
-            
+  return (
+    <div className="feed">
+      <div className="feed__inputContainer">
+        <div className="feed__input">
+          <CreateIcon />
+          <form>
+            <input type="text" />
+            <button type="submit">Send</button>
+          </form>
         </div>
-    );
-};
+        <div className="feed__inputOptions">
+          <InputOption title="Photo" Icon={ImageIcon} color="#70B5F9" />
+          <InputOption title="Video" Icon={SubscriptionsIcon} color="#E7A33E" />
+          <InputOption title="Event" Icon={EventNoteIcon} color="#C0CBCD" />
+          <InputOption
+            title="Write article"
+            Icon={CalendarViewDayIcon}
+            color="#7FC15E"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Feed;
--- src/InputOption.js
+++ src/InputOption.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import "./InputOption.css";
 
-function InputOption() {
+function InputOption( {Icon,title,color}) {
     return (
         <div className="inputOption">
+        <Icon style={{color: color}}/>
+        <h4>{title}</h4>
             
         </div>
     );
