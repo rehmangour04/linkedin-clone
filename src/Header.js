7a8cea6da9025e1ce0aaa8de937e6ab4a6012e2e
rehman <rehmangour04@gmail.com>
2021-11-06 11:06:14 +0530

    added avatar in header

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './Header.css';
 import SearchIcon from '@mui/icons-material/Search';
+import HeaderOption from './HeaderOption';
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+
 
 function Header() {
     return (
-        <>
         <div className='header'>
         <div className="header__left">
         <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt=""/>
@@ -18,10 +24,16 @@ function Header() {
         </div>
 
            <div className="header__right">
+           <HeaderOption Icon={HomeIcon} title="Home"/>
+           <HeaderOption  Icon ={SupervisorAccountIcon} title="My Network"/>
+           <HeaderOption Icon={BusinessCenterIcon} title="jobs"/>
+           <HeaderOption Icon={ChatIcon} title="Messaging" />
+           <HeaderOption Icon={NotificationsIcon} title="Notifications"/>
+
 
            </div>            
         </div>
-        </>
+        
     );
 }
 
--- src/HeaderOption.js
+++ src/HeaderOption.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import "./HeaderOption.css";
 
-const HeaderOption = () => {
+function HeaderOption({ avatar,Icon,title}){
     return (
-        <div className="headerOption">
-            
-        </div>
+    <div className="headerOption">
+    {Icon && <Icon className="headerOption__icon"/>
+    }
+
+    <h3 className='headerOption__title'>{title} </h3>
+
+    </div>
     );
-};
+}
 
 export default HeaderOption;
