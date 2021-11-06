f99745216e015de742bffd84dc043014bdaf4c93
rehman <rehmangour04@gmail.com>
2021-11-06 21:17:40 +0530

    building the input section

--- src/Feed.js
+++ src/Feed.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import "./Feed.css";
+import CreateIcon from '@mui/icons-material/Create';
+import InputOption from './InputOption';
 
-const Feed = () => {
+function Feed() {
     return (
-        <div>
+        <div className="feed">
+       <div className="feed__inputContainer">
+       <div className="feed__input">
+           <CreateIcon/>
+           <form>
+           <input type="text" />
+           <button type="submit">Send</button>
+
+
+           </form>
+
+       </div>
+       <div  className="feed__inputOptions">
+       <InputOption/>
+
+
+       </div>
+
+       </div>
             
         </div>
     );
---
+++ src/InputOption.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./InputOption.css";
+
+function InputOption() {
+    return (
+        <div className="inputOption">
+            
+        </div>
+    );
+};
+
+export default InputOption;
